f2c4d00ac09d497da495a4fe9af1de27aad45784
tihuep <timon.hueppi@gmail.com>
2022-07-24 20:56:24 +0200

    spring initialization

---
+++ src/main/java/ch/timonhueppi/etg/dmx_gateway/DmxGatewayApplication.java
@@ -0,0 +1,13 @@
+package ch.timonhueppi.etg.dmx_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmxGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmxGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/timonhueppi/etg/dmx_gateway/DmxGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.timonhueppi.etg.dmx_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmxGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
