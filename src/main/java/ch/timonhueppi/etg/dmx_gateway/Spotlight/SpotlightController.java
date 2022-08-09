766e8b10cdafd94a93c47ef3a14d9404fe351b81
tihuep <timon.hueppi@gmail.com>
2022-08-09 16:13:03 +0200

    fixes access to cross origin

--- src/main/java/ch/timonhueppi/etg/dmx_gateway/Spotlight/SpotlightController.java
+++ src/main/java/ch/timonhueppi/etg/dmx_gateway/Spotlight/SpotlightController.java
@@ -11,31 +11,34 @@ public class SpotlightController {
 
     private byte brightness = 0;
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/brightness")
     public byte getBrightness() {
         return brightness;
     }
 
+    @CrossOrigin(origins = "*")
     @PostMapping("/brightness/{brightness}")
     public void setBrightness(@PathVariable byte brightness) {
         this.brightness = brightness;
         spotlightService.setBrightness(brightness);
+        System.out.println("brightness: " + brightness);
     }
 
 
     private byte temperature = 0;
 
+    @CrossOrigin(origins = "*")
     @GetMapping("/temp")
     public byte getTemperature() {
-        return brightness;
+        return temperature;
     }
 
+    @CrossOrigin(origins = "*")
     @PostMapping("/temp/{temp}")
     public void setTemperature(@PathVariable byte temp) {
         this.temperature = temp;
         spotlightService.setTemperature(temp);
+        System.out.println("temp: " + temp);
     }
-
-
-
 }
