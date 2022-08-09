f1f60e54f1035667becd678db871c16c737b9ecf
tihuep <timon.hueppi@gmail.com>
2022-08-09 16:13:54 +0200

    adds endpoints to big color lights

---
+++ src/main/java/ch/timonhueppi/etg/dmx_gateway/ColorBig/ColorBigService.java
@@ -0,0 +1,36 @@
+package ch.timonhueppi.etg.dmx_gateway.ColorBig;
+
+import ch.timonhueppi.etg.dmx_gateway.Helper;
+
+public class ColorBigService {
+
+    public void setBrightness(byte brightness){
+        handleSetter(brightness, -1);
+    }
+
+    public void setRed(byte red){
+        handleSetter(red, 0);
+    }
+
+    public void setGreen(byte green){
+        handleSetter(green, 1);
+    }
+
+    public void setBlue(byte blue){
+        handleSetter(blue, 2);
+    }
+
+    public void setWhite(byte white){
+        handleSetter(white, 3);
+    }
+
+    private void handleSetter(byte value, int offset){
+        int numberOfFixtures = Integer.valueOf(Helper.getProperty("colorbig.number"));
+        int startChannel = Integer.valueOf(Helper.getProperty("colorbig.channel.start"));
+        byte[] dmxData = Helper.dmxData;
+        for (int i = 0; i < numberOfFixtures; i++){
+            dmxData[(startChannel + i*6) + offset] = value;
+        }
+        Helper.sendDMX(dmxData);
+    }
+}
